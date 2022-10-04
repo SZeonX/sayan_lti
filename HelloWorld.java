91f9a571757b469d7026f8acada3c49f3aaf39db
SZeonX <91210331+SZeonX@users.noreply.github.com>
2022-10-04 16:38:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("My name is SAYAN.\nWho the hell are you ?");
+    System.out.println("My name is SAYAN.");
   }
 }
